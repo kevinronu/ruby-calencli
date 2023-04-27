b55144a114784998fcb2cb9c35c7e9cbd6a1c873
kevinronu <kevinronu@gmail.com>
2023-04-26 22:35:38 -0500

    add check_correct_hours method

--- calencli_methods.rb
+++ calencli_methods.rb
@@ -1,3 +1,4 @@
+require "date"
 # Methods
 
 def check_empty(input, text, msg = "")
@@ -9,8 +10,20 @@ def check_empty(input, text, msg = "")
   input
 end
 
-def check_valid_hour(new_input)
-  return true if new_input.empty? || new_input.match?(/^(?:[01]\d|2[0-3]):[0-5]\d$/)
+def check_valid_hour(input)
+  return true if input.empty? || input.match?(/^(?:[01]\d|2[0-3]):[0-5]\d$/)
 
   false
+end
+
+def check_correct_hours(dual_input)
+  return true if dual_input.empty?
+
+  a = dual_input[0..4]
+  b = dual_input[6..10]
+  # DateTime.strptime('02/22/2018 5:20 PM', '%m/%d/%Y %l:%M %p')
+  date1 = DateTime.strptime("2023-04-26 #{a}", "%Y-%m-%d %H:%M")
+  date2 = DateTime.strptime("2023-04-26 #{b}", "%Y-%m-%d %H:%M")
+
+  date1 < date2
 end
